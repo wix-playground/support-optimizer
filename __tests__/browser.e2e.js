62667fdb940004f34ea46e7762606a995436df32
dariiad <darriad@wix.com>
2019-04-12 17:44:10 +0300

    Costs, Modal, TimeZone

--- __tests__/browser.e2e.js
+++
@@ -1,8 +0,0 @@
-describe('React application', () => {
-  it('should display title', async () => {
-    await page.goto(app.getUrl('/'));
-    await page.waitForSelector('h2');
-
-    expect(await page.$eval('h2', e => e.innerText)).toEqual('Hello World!');
-  });
-});
