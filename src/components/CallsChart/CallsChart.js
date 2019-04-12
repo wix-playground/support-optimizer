23d6d0564b16862dd2f24c92dc9ce80b5f84b838
Rostyslav <rostyslavp@wix.com>
2019-04-12 16:15:40 +0300

    change charts

--- src/components/CallsChart/CallsChart.js
+++ src/components/CallsChart/CallsChart.js
@@ -8,20 +8,12 @@ import {
   Tooltip,
   XAxis,
   YAxis,
+  Line,
+  LineChart
 } from 'recharts';
-import { Col } from 'wix-style-react/dist/src/Grid';
+import { Col, Row} from 'wix-style-react/dist/src/Grid';
 import Loader from 'wix-style-react/Loader';
 import { convertDate } from '../../utils';
-
-const sampleData = [
-  {name: '10-11', max: 4000, min: 2400, mid: 2400},
-  {name: '11-12', max: 3000, min: 1398, mid: 2210},
-  {name: '12-13', max: 2000, min: 9800, mid: 2290},
-  {name: '14-15', max: 2780, min: 3908, mid: 2000},
-  {name: '15-16', max: 1890, min: 4800, mid: 2181},
-  {name: '16-17', max: 2390, min: 3800, mid: 2500},
-  {name: '17-18', max: 3490, min: 4300, mid: 2100},
-];
 
 class CallsChart extends React.PureComponent {
   constructor() {
@@ -51,25 +43,44 @@ class CallsChart extends React.PureComponent {
 
   render() {
     if (this.state.isLoading) {
-      return <Loader />;
+        return <Loader />;
     }
     return (
       <Col span={7}>
-        <BarChart
-          width={500}
-          height={300}
-          data={sampleData}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="min" stackId="a" fill="#e25d47" />
-          <Bar dataKey="max" stackId="a" fill="#eb8404" />
-          <Bar dataKey="mid" stackId="a" fill="#199384" />
-        </BarChart>
+        <Row>
+          <Row>Selected date - {convertDate(this.props.selectedDate)}</Row>
+          <LineChart
+            width={500}
+            height={300}
+            data={this.state.callsData.calls}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="hour" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{ r: 6 }} />
+          </LineChart>
+        </Row>
+
+        <Row>
+          <BarChart
+            width={500}
+            height={300}
+            data={this.state.callsData.support}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="hour" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" stackId="count" fill="#e25d47" />
+          </BarChart>
+        </Row>
       </Col>
     );
   }
--- src/components/TicketsChart/TicketsChart.js
+++ src/components/TicketsChart/TicketsChart.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import { translate } from 'react-i18next';
-import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from 'recharts';
-import {Col} from 'wix-style-react/dist/src/Grid';
+import {Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts';
+import { Col, Row } from 'wix-style-react/dist/src/Grid';
 import Loader from 'wix-style-react/Loader';
-import { convertDate } from "../../utils";
+import { convertDate } from '../../utils';
 
-const sampleData = [
-  {name: '10-11', max: 4000, min: 2400, mid: 2400},
-  {name: '11-12', max: 3000, min: 1398, mid: 2210},
-  {name: '12-13', max: 2000, min: 9800, mid: 2290},
-  {name: '14-15', max: 2780, min: 3908, mid: 2000},
-  {name: '15-16', max: 1890, min: 4800, mid: 2181},
-  {name: '16-17', max: 2390, min: 3800, mid: 2500},
-  {name: '17-18', max: 3490, min: 4300, mid: 2100},
-];
 
 class CallsChart extends React.Component {
   constructor() {
@@ -31,7 +22,8 @@ class CallsChart extends React.Component {
     this.setIsLoading(true);
     const convertedDateToRightFormat = convertDate(this.props.selectedDate);
 
-    const { data: ticketsData } = await this.props.fetcher.getTickets(convertedDateToRightFormat);
+    const { data: ticketsData } =
+      await this.props.fetcher.getTickets(convertedDateToRightFormat);
 
     this.setState({
       ticketsData,
@@ -45,21 +37,41 @@ class CallsChart extends React.Component {
     }
     return (
       <Col span={7}>
-        <BarChart
-          width={500}
-          height={300}
-          data={sampleData}
-          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="min" stackId="a" fill="#e25d47" />
-          <Bar dataKey="max" stackId="a" fill="#eb8404" />
-          <Bar dataKey="mid" stackId="a" fill="#199384" />
-        </BarChart>
+        <Row>Selected date - {convertDate(this.props.selectedDate)}</Row>
+        <Row>
+
+          <LineChart
+            width={500}
+            height={300}
+            data={this.state.ticketsData.tickets}
+            margin={{
+              top: 5, right: 30, left: 20, bottom: 10,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis label="Hours" dataKey="hour" />
+            <YAxis label="Count"/>
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{ r: 6 }} />
+          </LineChart>
+        </Row>
+
+        <Row>
+          <BarChart
+            width={500}
+            height={300}
+            data={this.state.ticketsData.support}
+            margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="hour" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" stackId="count" fill="#e25d47" />
+          </BarChart>
+        </Row>
       </Col>
     );
   }
