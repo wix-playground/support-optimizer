0a3a0958f5705193db721ff067045a69773f80d4
Rostyslav <rostyslavp@wix.com>
2019-04-15 11:35:02 +0300

    change order of bars

--- src/components/TicketsChart/TicketsChart.js
+++ src/components/TicketsChart/TicketsChart.js
@@ -59,9 +59,9 @@ class CallsChart extends React.Component {
             <YAxis />
             <Tooltip />
             <Legend />
+            <Line type="monotone" dataKey="min_count" stroke="#f29f2b" activeDot={{ r: 5 }} />
             <Line type="monotone" dataKey="count" stroke="#8884d8" activeDot={{ r: 6  }} />
             <Line type="monotone" dataKey="max_count" stroke="#f22b46" activeDot={{ r: 5 }} />
-            <Line type="monotone" dataKey="min_count" stroke="#f29f2b" activeDot={{ r: 5 }} />
           </LineChart>
 
         </Col>
@@ -79,9 +79,9 @@ class CallsChart extends React.Component {
             <YAxis />
             <Tooltip />
             <Legend />
+            <Bar dataKey="min_count" stackId="count" fill="#f29f2b" />
             <Bar dataKey="count" stackId="count" fill="#8884d8" />
             <Bar dataKey="max_count" stackId="count" fill="#f22b46" />
-            <Bar dataKey="min_count" stackId="count" fill="#f29f2b" />
           </BarChart>
         </Col>
       </Row>
